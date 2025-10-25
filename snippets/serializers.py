e7721ca72084dd4b86ef3068abd370ccb73af0a7
shammas-nasar <shammas.nasar@panasatech.com>
2025-10-25 15:52:56 +0530

    In serializer add SnippetOverviewSerializer for overview api response and add docs for SnippetSerializer. In views add overview method and update destroy method for return remaining list after deletion.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,22 +2,32 @@ from rest_framework import serializers
 from .models import Snippet, Tag
 
 
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ['id', 'title']
-
-
 class SnippetSerializer(serializers.ModelSerializer):
+    """
+    Serialize Snippet model data for CRUD operations.
+
+    Used in:
+    - SnippetViewSet for create, retrieve, update, and destroy actions.
+
+    Fields:
+    - id: Unique identifier of the snippet.
+    - title: Title of the snippet.
+    - note: The main text content of the snippet.
+    - created_at (datetime): Timestamp when the record was created (read-only)
+    - updated_at (datetime): Timestamp when the record was last updated
+        (read-only)
+    - tags (list): List of tag titles.
+
+    """
     tags = serializers.ListField(write_only=True)
 
     class Meta:
         model = Snippet
         fields = [
             'id', 'title', 'note', 'created_at',
-            'updated_at', 'tags', 'created_by'
+            'updated_at', 'tags',
         ]
-        read_only_fields = ['created_at', 'updated_at', 'created_by']
+        read_only_fields = ['created_at', 'updated_at']
 
     def create(self, validated_data):
         tags_data = validated_data.pop('tags', [])
@@ -50,3 +60,23 @@ class SnippetSerializer(serializers.ModelSerializer):
             instance.tags.values_list('title', flat=True)
         )
         return representation
+
+
+class SnippetOverviewSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Serializer for snippet overview.
+
+    Used in:
+    - SnippetViewSet overview action.
+
+    Fields:
+    - id: Unique identifier of the snippet.
+    - title: Title of the snippet.
+    - url: Hyperlinked URL to the snippet detail view.
+    """
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'url']
+        extra_kwargs = {
+            'url': {'view_name': 'snippet-detail', 'lookup_field': 'pk'}
+        }
--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,31 @@
 from rest_framework import viewsets
 from django.db import transaction
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, SnippetOverviewSerializer
 
 
-class SnippetViewSet(viewsets.ModelViewSet):
+class SnippetViewSet(
+    viewsets.ViewSet,
+    CreateAPIView,
+    RetrieveUpdateDestroyAPIView
+):
+    """
+    ModelViewSet for managing user snippets.
+
+    overview: Returns total_snippets and snippets with title and url
+        to its details which are created by the authenticated user.
+    create: Creates a new snippet and get_or_create tags and links it.
+    retrieve: Returns snippet details.
+    update: Updates snippet data.
+    destroy: Deletes snippet and returns updated list.
+    """
     serializer_class = SnippetSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Snippet.objects.filter(
@@ -17,3 +36,22 @@ class SnippetViewSet(viewsets.ModelViewSet):
         with transaction.atomic():
             snippet = serializer.save(created_by=self.request.user)
             return snippet
+
+    @action(detail=False, methods=['get'], url_path='overview')
+    def overview(self, request, *args, **kwargs):
+        snippets = self.get_queryset()
+        serializer = SnippetOverviewSerializer(
+            snippets,
+            many=True,
+            context={'request': request}
+        )
+        return Response({
+            "total_count": snippets.count(),
+            "snippets": serializer.data
+        })
+
+    def destroy(self, request, *args, **kwargs):
+        super().destroy(request, *args, **kwargs)
+        snippets = self.get_queryset()
+        serializer = self.get_serializer(snippets, many=True)
+        return Response(serializer.data)
